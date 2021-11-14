a9ecec2c121cfc986f335ae8535c8edf8c66df9a
Douglas Gomes <douglasgommez@gmail.com>
2021-11-14 12:10:46 -0300

    Created Sealed class Screen.kt

--- app/src/main/java/com/odougle/custombottomnavigation/MainActivity.kt
+++ app/src/main/java/com/odougle/custombottomnavigation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import com.odougle.custombottomnavigation.ui.theme.CustomBottomNavigationTheme
 
@@ -15,24 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CustomBottomNavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
+                window.statusBarColor = MaterialTheme.colors.background.toArgb()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CustomBottomNavigationTheme {
-        Greeting("Android")
-    }
-}
